dbaf17ae07ed011f15c059c02b9b3f86e0732dc5
harish0585 <harish.1994.19@gmail.com>
2022-07-01 00:25:54 +0530

    new changes

--- src/components/WeatherAPI.jsx
+++ src/components/WeatherAPI.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getApiData } from '../redux/actions/weatherapiActions'
+import styles from "./Weather.module.css";
 
 const WeatherAPI = () => {
   
@@ -15,14 +16,23 @@ const WeatherAPI = () => {
      setCity("");
   }
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+     dispatch(getApiData(city));
+     setCity("");
+    }
+  }
+
   return (
-    <div>
+    <>
       <h1>Weather API</h1>
-      <input type="text" value={city} placeholder='Enter city name' onChange={(e) => setCity(e.target.value)} />
-      <button onClick={handleData}>Search</button>
+      <span className={styles.place}> <i className="fa fa-map-marker" aria-hidden="true"></i></span>
+     <span className={styles.find} onClick={handleData}><i className="fa fa-search"></i></span>
+      <input type="text" value={city} className={styles.enter} placeholder='Enter city name' onChange={(e) => setCity(e.target.value)} onKeyDown={(event) => handleKeyDown(event)}/>
+      {/* <button onClick={handleData}>Search</button> */}
       <div>{data?.name}</div>
       <div>{data?.main?.temp}</div>
-    </div>
+    </>
   )
 }
 
